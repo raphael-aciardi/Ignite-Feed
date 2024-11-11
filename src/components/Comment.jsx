875924e335fe7fbb4b8598c7b3747610698784f3
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2024-11-11 15:31:31 -0300

    Finalizing the comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,36 @@
+import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 
 export function Comment() {
   return (
-    <div>a</div>
+    <div className={styles.comment}>
+      <img src="https://github.com/raphael-aciardi.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Raphael Aciardi</strong>
+              <time title="11 de Maio de 2022" dateTime="2022-05-11 19:00:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+            
+          </header>
+
+          <p>Muito bom Raphael, parabéns!! </p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+
+      </div>
+    </div>
   )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,50 +1,59 @@
 import styles from "./Post.module.css";
+import { Comment } from "./Comment";
 
 export function Post() {
-  return (
-    <article className={styles.post}>
-      <header> 
-        <div className={styles.author}>
-          <img 
-            className={styles.avatar}
-            src="https://github.com/raphael-aciardi.png"
-          />
-          <div className={styles.authorInfo}>
-            <strong>Raphael Aciardi</strong>
-            <span>Web Developer</span>
-          </div>
-        </div>
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img
+                        className={styles.avatar}
+                        src="https://github.com/raphael-aciardi.png"
+                    />
+                    <div className={styles.authorInfo}>
+                        <strong>Raphael Aciardi</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
 
-        <time title="11 de Maio de 2022" dateTime="2022-05-11 19:00:00">
-          Publicado há 1h
-        </time>
-      </header>
+                <time title="11 de Maio de 2022" dateTime="2022-05-11 19:00:00">
+                    Publicado há 1h
+                </time>
+            </header>
 
-      <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
 
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
+                <p>
+                    Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+                    no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+                </p>
 
-        <p>👉jane.design/doctorcare</p>
+                <p>👉jane.design/doctorcare</p>
 
-        <p>
-          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
-          <a href="#">#rocketseat</a>{" "}
-        </p>
-      </div>
+                <p>
+                    <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+                    <a href="#">#rocketseat</a>{" "}
+                </p>
+            </div>
 
-      <form className={styles.commentForm}>
-        <strong>Deixe seu feedback</strong>
-        <textarea
-          placeholder="Deixe um comentário"
-        />
-        <footer>
-          <button type="submit">Comentar</button>
-        </footer>
-      </form>
-    </article>
-  );
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                    placeholder="Deixe um comentário"
+                />
+                <footer>
+                    <button type="submit">Comentar</button>
+                </footer>
+            </form>
+
+           <div>
+                <Comment />
+                <Comment />
+                <Comment />
+           </div>
+
+
+        </article>
+    );
 }
